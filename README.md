ed6f87a54cb7dc6f7d733f5c8c477157804a4d14
Daniel Antonio <daniel.conte@stillfront.com>
2024-06-20 19:59:44 -0300

    primeiro commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # flask-exemplo
+
+
+$ pip install Flask
+
+$ git clone https://github.com/uniarp/flask-exemplo.git
+
+$ flask --version
+$ flask --app teste app
---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# http://127.0.0.1:5000/soma?a=10&b=15
+@app.route('/') # url, path, rota, caminho, link, uri
+def inicio():
+    resultado =  {
+        "Nome": "Daniel"
+    }
+    return [resultado]
