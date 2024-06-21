47fba7d711ca2b3e6eae57ad983eaeb2d218131a
michael <michaelreques@gmail.com>
2024-06-20 21:26:13 -0300

    route-historico-clinico

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# http://127.0.0.1:5000/soma?a=10&b=15
-@app.route('/') # url, path, rota, caminho, link, uri
+# http://127.0.0.1:5000/
+
+@app.route('/historico_clinico') # url, path, rota, caminho, link, uri
 def inicio():
     resultado =  {
-        "Nome": "Daniel"
+        "Nome_tutor": "Daniel",
+        "nome_animal": "Bolo",
+        "data_agendada": "24/04/2019",
     }
     return [resultado]
