c2715f86d72dd0afe65c50cec1979798f525a705
Gustavo <gustavomiguel0312@gmail.com>
2024-06-20 21:40:55 -0300

    Cadastrar Tutor - Gustavo

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-# http://127.0.0.1:5000/soma?a=10&b=15
-@app.route('/') # url, path, rota, caminho, link, uri
-def inicio():
-    resultado =  {
-        "Nome": "Gustavo"
-    }
+
+# http://127.0.0.1:5000/cadastrarTutor
+@app.route('/cadastrarTutor')
+def cadastrarTutor():
+    resultado = {
+        "id_tutor": "321",
+        "nome": "Gustavo Miguel",
+        "endereço": "Rua Avenidade Paulista, N70",
+        "telefone": "49912345678",
+        "email": "gustavomiguel0312@gmail.com"
+        }
     return [resultado]
