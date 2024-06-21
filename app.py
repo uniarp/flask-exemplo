a54b522a4b393dd2237e946023d7dd5587051ca3
Kennedy-Gustavo <kennedyoliveira2108@gmail.com>
2024-06-21 02:07:48 -0300

    Trabalho API

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # http://127.0.0.1:5000/soma?a=10&b=15
-@app.route('/') # url, path, rota, caminho, link, uri
+@app.route('/cadastrar_Doencas') # url, path, rota, caminho, link, uri
 def inicio():
     resultado =  {
-        "Nome": "Daniel"
+        "id_doenca": "123",
+        "nome_doença": "Ehrlichiose",
+        "sintomas": "apatia, febre, vômito",
+        "tratamentos_associado": "antibióticos",
     }
     return [resultado]
