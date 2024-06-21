c2e3b128df83503fec850e59cab1e52f90cd7ab9
Fernando Pereira Monteiro <002456@uniarp.edu.br>
2024-06-20 21:27:08 -0300

    FERNANDO-FUNÇÃO ANIMAL

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # http://127.0.0.1:5000/soma?a=10&b=15
-@app.route('/') # url, path, rota, caminho, link, uri
-def inicio():
-    resultado =  {
-        "Nome": "Daniel"
-    }
+@app.route('/animal') # url, path, rota, caminho, link, uri
+def animal():
+    resultado = {
+        'nome_animal': "Carreta",
+        'especie': "cachoro",
+        'raca': "São Bernado",
+        'peso': "67 Kg",
+        'idade': "2 anos",
+        'rfid': "1711813393"
+        }
     return [resultado]
