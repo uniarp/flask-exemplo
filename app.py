f402c22d6777113ac89e592cd7319f39c9f17604
Gabriel Dalpiaz <gabriel666dalpiaz@gmail.com>
2024-06-20 21:40:26 -0300

    gabrielD

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route('/AnimalParaAdocao') # URL, path, rota, caminho, link, URI
-def get_animal_info():
+def animal():
     resultado = {
         "nomeDoAnimal": "pitoco",
         "especieDoAnimal": "felino",
