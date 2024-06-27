8140814cc41d476fae5a0187ee71c11d3500a99b
Daniel Antonio <daniel.conte@stillfront.com>
2024-06-27 20:23:19 -0300

    correcao

--- app.py
+++ app.py
@@ -22,7 +22,8 @@ def AnimalParaAdocao():
         "corPredominanteDoAnimal": "preto",
         "dadosDeNascimentoDoAnimal": "15/05/1997",
         "racaDoAnimal": "Lulu da pomerânia"
-
+    }
+    return resultado
 # http://127.0.0.1:5000/
 
 @app.route('/historico_clinico') # url, path, rota, caminho, link, uri
@@ -31,7 +32,8 @@ def historico_clinico():
         "Nome_tutor": "Daniel",
         "nome_animal": "Bolo",
         "data_agendada": "24/04/2019",
-
+    }
+    return [resultado]
 # http://127.0.0.1:5000/soma?a=10&b=15
 @app.route('/cadastrar_Doencas') # url, path, rota, caminho, link, uri
 def inicio():
@@ -39,8 +41,9 @@ def inicio():
         "id_doenca": "123",
         "nome_doença": "Ehrlichiose",
         "sintomas": "apatia, febre, vômito",
-        "tratamentos_associado": "antibióticos",
-
+        "tratamentos_associado": "antibióticos"
+    }
+    return [resultado]
 @app.route('/animal') # url, path, rota, caminho, link, uri
 def animal():
     resultado = {
@@ -51,6 +54,7 @@ def animal():
         'idade': "2 anos",
         'rfid': "1711813393"
         }
+    return [resultado]
 
 @app.route('/tutor') # url, path, rota, caminho, link, uri
 def tutor():
