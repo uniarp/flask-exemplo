c337c07577f2dc3815214508b36d427dbdf815cb
Gabriel Kowalczyk <gabriel.gruckowski@gmail.com>
2024-06-27 20:12:08 -0300

    gabriel

--- app.py
+++ app.py
@@ -9,3 +9,15 @@ def inicio():
         "Nome": "Daniel"
     }
     return [resultado]
+
+@app.route('/cadastrartutor')
+def salvar():
+    resultado = {
+        "nome_animal" : "Carreta",
+        "especie" : "cachoro",
+        "raca" : "São Bernado",
+        "peso" : "67 Kg",
+        "idade" : "2 anos",
+        "rfid" : "1711813393"
+    }
+    return [resultado]
