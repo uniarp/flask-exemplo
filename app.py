0a4fac2482f81c3238d66baf1ff54e954224f9bb
Luis Otávio <luispassos223@gmail.com>
2024-06-27 20:16:59 -0300

    MinhaAPI

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # http://127.0.0.1:5000/soma?a=10&b=15
-@app.route('/') # url, path, rota, caminho, link, uri
-def inicio():
+@app.route('/Denuncia') # url, path, rota, caminho, link, uri
+def denuncia():
     resultado =  {
-        "Nome": "Daniel"
+        "Endereço" : "Caçador SC",
+        "Bairro" : "Gioppo",
+        "Observação" : "Maus Tratos"
     }
     return [resultado]
