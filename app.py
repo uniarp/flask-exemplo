3228b89b05272f5b677af7bdda2b6d9ff3d6ffb6
HenryKaum <hccm503@gmail.com>
2024-06-27 21:35:56 -0300

    cadastrar animal

--- app.py
+++ app.py
@@ -105,3 +105,18 @@ def salvar():
         "rfid" : "1711813393"
     }
     return [resultado]
+
+
+@app.route('/cadastrar/animal/')
+def consulta():
+    resultado = {
+    {"nome_aniaml": "Tusk"},
+    {"especie": "Cachorro"},
+    {"raça": "Pastor-Alemão"},
+    {"sexo": "Macho"},
+    {"peso": "30Kg"},
+    {"idade_estimada": "7 anos"},
+    {"historico_clinico": "Animal com vacinação em dia"}
+    }
+    return [resultado]
+
