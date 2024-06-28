3773ee6e20a89a23d576008239ed54aabab65346
Luis Otávio <luispassos223@gmail.com>
2024-06-27 21:20:02 -0300

    LuisOtavio2

--- app.py
+++ app.py
@@ -3,9 +3,22 @@ from flask import Flask, request
 app = Flask(__name__)
 
 # http://127.0.0.1:5000/soma?a=10&b=15
-@app.route('/') # url, path, rota, caminho, link, uri
-def inicio():
+@app.route('/divulgacao') # url, path, rota, caminho, link, uri
+def campanhaAnimal1():
     resultado =  {
-        "Nome": "Daniel"
+        "Dados do Evento": "20/09/2024",
+        "Horário do Evento" : "15:00",
+        "Local do Evento" : "Parque Central",
+        "Imagem" : "Image.png"
+    }
+    return [resultado]
+
+@app.route('/divulgacao2')
+def campanhaAnimal2():
+    resultado =  {
+        "Dados do Evento": "25/07/2024",
+        "Horário do Evento" : "14:00",
+        "Local do Evento" : "Parque Das Araucarias",
+        "Imagem" : "Image.png"
     }
     return [resultado]
